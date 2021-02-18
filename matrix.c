1d7559e097d8bd2a28f9d62e26357821092a748d
David Mallasen <davidmq@kth.se>
2021-02-18 20:23:49 +0100

    Change input parameters to single pointers
    In order to accomodate to the calls from Fortran

--- matrix.c
+++ matrix.c
@@ -1,15 +1,14 @@
 #include "matrix.h"
 #include <stdio.h>
 
-void matrix_multiply(int n, int** c, int** a, int** b) {
+void matrix_multiply(int n, int* c, int* a, int* b) {
     printf("Matrix multiply in c!\n");
-    printf("n %d", n);
-    //printf("Inputs: c %d a %d b %d", c[0][0], a[0][0], b[0][0]);
+
     for(int i = 0; i < n; ++i) {
         for(int j = 0; j < n; ++j) {
-            c[i][j] = 0;
+            c[i * n + j] = 0;
             for(int k = 0; k < n; ++k) {
-                c[i][j] += a[i][k] * b[k][j];
+                c[i * n + j] += a[i * n + k] * b[k * n + j];
             }
         }
     }
--- matrix.h
+++ matrix.h
@@ -1,6 +1,6 @@
 #ifndef MATRIX_H_
 #define MATRIX_H_
 
-void matrix_multiply(int n, int** c, int** a, int** b); 
+void matrix_multiply(int n, int* c, int* a, int* b); 
 
 #endif  // MATRIX_H_
